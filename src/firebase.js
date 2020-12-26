44722ea5084373173c17e5e6e17c0e87e7c98bc6
mak-maak <azizk7693@gmail.com>
2020-12-26 22:03:29 +0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA-bYeDpnr2YFTugfYFHf7uiTaYVpjYfxk",
+    authDomain: "facebook-clone-e5e2a.firebaseapp.com",
+    projectId: "facebook-clone-e5e2a",
+    storageBucket: "facebook-clone-e5e2a.appspot.com",
+    messagingSenderId: "261908818571",
+    appId: "1:261908818571:web:5a93a017766c12c21c428e"
+  };
